f27d6a14284fcd415c6dd1c4e8daaedc4ef0cb04
nguyenthanhnhannn <nguyenthanhnhan28082005@gmail.com>
2024-08-25 23:29:52 +0700

    Add files via upload

---
+++ doiragiay.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 24 14:04:52 2024
+
+@author: Hi
+"""
+
+a = int(input("Nhập giờ: " ))
+b = int(input("Nhập phút: "))
+c = int(input("Nhập giây: "))
+giay = (a * 3600) + (b * 60) + c 
+print("Kết quả đổi ra giây là:" ,giay)
+
